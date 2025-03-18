526fa0a81291a8b3f71afcac521c8e8d4e2b9e09
Revanthsatyam <satyamrevanth@gmail.com>
2025-03-18 18:21:40 +0530

    changes

--- main.tf
+++ main.tf
@@ -35,15 +35,18 @@ resource "aws_docdb_cluster_parameter_group" "main" {
 }
 
 resource "aws_docdb_cluster" "docdb" {
-  cluster_identifier      = "${local.name_prefix}-cluster"
-  engine                  = var.engine
-  engine_version          = var.engine_version
-  master_username         = "foo"
-  master_password         = "mustbeeightchars"
-  backup_retention_period = var.backup_retention_period
-  preferred_backup_window = var.preferred_backup_window
-  skip_final_snapshot     = var.skip_final_snapshot
-  tags                    = merge(local.tags, { Name = "${local.name_prefix}-cluster" })
+  cluster_identifier              = "${local.name_prefix}-cluster"
+  engine                          = var.engine
+  engine_version                  = var.engine_version
+  master_username                 = "foo"
+  master_password                 = "mustbeeightchars"
+  backup_retention_period         = var.backup_retention_period
+  preferred_backup_window         = var.preferred_backup_window
+  skip_final_snapshot             = var.skip_final_snapshot
+  vpc_security_group_ids          = [aws_security_group.main.id]
+  db_cluster_parameter_group_name = aws_docdb_cluster_parameter_group.main.name
+  db_subnet_group_name            = aws_docdb_subnet_group.main.id
+  tags                            = merge(local.tags, { Name = "${local.name_prefix}-cluster" })
 }
 
 resource "aws_docdb_cluster_instance" "cluster_instances" {
